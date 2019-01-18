3c22ae5f1172e8238662c84015c2ef88a5c7ffc5
dimiork <dimiork@gmail.com>
2019-01-18 13:58:22 +0200

    add tests to localstorage service

--- src/app/services/localstorage.service.spec.ts
+++ src/app/services/localstorage.service.spec.ts
@@ -3,10 +3,52 @@ import { TestBed } from '@angular/core/testing';
 import { LocalstorageService } from './localstorage.service';
 
 describe('LocalstorageService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: LocalstorageService;
+  const data = [{ id: 1, title: 'Title 1', items: [{ title: 'item name', description: 'item description'}]}];
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [ LocalstorageService ]
+    })
+    service = TestBed.get(LocalstorageService);
+    let store = {};
+    const mockLocalStorage = {
+      getItem: (key: string): string => {
+        return key in store ? store[key] : null;
+      },
+      setItem: (key: string, value: string) => {
+        store[key] = `${value}`;
+      },
+      removeItem: (key: string) => {
+        delete store[key];
+      },
+      clear: () => {
+        store = {};
+      }
+    };
+
+    spyOn(localStorage, 'getItem')
+      .and.callFake(mockLocalStorage.getItem);
+    spyOn(localStorage, 'setItem')
+      .and.callFake(mockLocalStorage.setItem);
+    spyOn(localStorage, 'removeItem')
+      .and.callFake(mockLocalStorage.removeItem);
+    spyOn(localStorage, 'clear')
+      .and.callFake(mockLocalStorage.clear);
+  });
 
   it('should be created', () => {
-    const service: LocalstorageService = TestBed.get(LocalstorageService);
     expect(service).toBeTruthy();
+  });
+
+  it('should store lists in localStorage',
+    () => {
+      service.save(data);
+      expect(localStorage.getItem(service.name)).toEqual(JSON.stringify(data));
+  });
+
+  it('should return stored lists from localStorage',
+    () => {
+      localStorage.setItem(service.name, JSON.stringify(data));
+      expect(service.load()).toEqual(data);
   });
 });
