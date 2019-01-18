a1f14b3fd85417c17a1da438826461ef4cecccad
dimiork <dimiork@gmail.com>
2019-01-18 13:20:42 +0200

    add minimal layout

---
+++ src/app/components/board/board.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.css']
+})
+export class BoardComponent implements OnInit {
+
+  public lists: any = [{id: 1, title: 'Title #1', items: [1, 2, 3]}, {id: 2, title: 'Title #2', items: [1, 2]}];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
