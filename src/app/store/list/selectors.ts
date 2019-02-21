7c12799d35c6e68fd02f0e02376594f73db71cba
dimiork <dimiork@gmail.com>
2019-02-21 11:58:11 +0200

    clean directories from unused files

--- src/app/store/list/selectors.ts
+++
@@ -1,7 +0,0 @@
-import { createSelector } from '@ngrx/store';
-
-import { AppState } from '../app.reducer';
-// tslint:disable
-export const getState = (state: AppState) => state;
-export const getLists = (state: AppState) => state.lists;
-// tslint:enable
